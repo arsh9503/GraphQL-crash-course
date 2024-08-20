ef3c742fdb0cf0ae789feefbe7d6ebd5dbec41e6
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-19 22:53:00 -0600

    chore: Configure Apollo Server and startStandaloneServer

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import { ApolloServer } from "@apollo/server";
+import { ApolloServer } from "@apollo/server"; 
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+//Server setup
+
+const server = new ApolloServer({
+    //typeDefs -- schema
+    
+    //Resolver functions -- resolvers property
+})
+
+const {url} = await startStandaloneServer(server, {
+    listen: {port: 4000}
+});
+
+console.log("Server ready at port", 4000);
