19a91609d91c9e9378a80dad4f8e73eecb2b96ec
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-19 23:12:13 -0600

    chore: Update Game type definition in schema.js

--- schema.js
+++ schema.js
@@ -1,12 +1,27 @@
 export const typeDefs = `#graphql 
     type Game {
-        id: ID!,
-        title: String!,
-        platform: [String]!
-    },
+        id: ID!;
+        title: String!;
+        platform: [String!]!;
+    }
 
-    
+    type Review {
+        id: ID!;
+        rating: Int!;
+        content: String!;
+    }
 
+    type Author {
+        id: ID!;
+        name: String!;
+        verified: Boolean!;
+    }
+
+    type Query {
+        reviews: [Review];
+        games: [Game];
+        authors: [Author];
+    }
 `
 
 //int, float, string, boolean, ID --> key for data objects
