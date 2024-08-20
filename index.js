d40c22b939e723c33326956de7cee7711e03d975
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-19 23:16:12 -0600

    chore: Import typeDefs from schema.js in ApolloServer configuration

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { ApolloServer } from "@apollo/server"; 
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+import { typeDefs } from "./schema.js";
 //Server setup
 
 const server = new ApolloServer({
     //typeDefs -- schema --> Definitions of different types of data we want to expose on our server
     // schema -- describes the shape of data sets
+    typeDefs,
     
 
     //Resolver functions -- resolvers property
